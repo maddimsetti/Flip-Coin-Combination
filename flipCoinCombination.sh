bbd3533334ce0c1fe73a18b508f28fdf0a6315f7
prudhvi <prudhvieethakota123@gmail.com>
2021-04-16 21:02:15 +0530

    Added:Loop through Multiple times of Flipping a coin Store the Singlet Combination In a Dictionary. Finally determine the percentage of Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,12 +2,29 @@
 
 echo "Flip Coin Combination Problem"
 
-randomNumber=$((RANDOM%2))
-echo $randomNumber
+count=0
+for (( i=1; i<=10; i++ ))
+do
+   singlet[ ((count++)) ]=$((RANDOM%2))
+done
+   echo ${singlet[@]}
+   echo ${!singlet[@]}
 
-if [ $randomNumber -eq 1 ]
-then
-    echo "Head"
-else 
-    echo "Tail"
-fi
+
+for(( i=1; i<=10; i++ ))
+do
+   if [ ${singlet[i]} -eq 1 ]
+   then
+       ((Head++))
+   else
+       ((Tail++))
+   fi
+done
+
+echo "Number of Heads Win = $Head"
+echo "Number of Tails Win = $Tail"
+
+HeadPercentage=$((100*$Head/10))
+echo $HeadPercentage
+TailPercentage=$((100*$Tail/10))
+echo $TailPercentage
