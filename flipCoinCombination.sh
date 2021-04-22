34ca45485f12d68293c0a1bc38161f68da7cd134
prudhvi <prudhvieethakota123@gmail.com>
2021-04-22 14:44:46 +0530

    Added:As a Simulator, loop to created a  Doublet Combination. Stored the doublet combination as well as the percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,28 +3,44 @@
 echo "Flip Coin Combination Problem"
 
 count=0
-for (( i=1; i<=10; i++ ))
+for (( i=1; i<=20; i++ ))
 do
-   singlet[ ((count++)) ]=$((RANDOM%2))
+   declare -A doublet
+   declare -A doublet1
+   doublet[ ((count++)) ]=$((RANDOM%2))
+   doublet1[ ((count++)) ]=$((RANDOM%2))
+
+        if [ ${doublet[@]} -eq 1 -a ${doublet1[@]} -eq 1 ]
+        then
+           echo "Head Head"
+           ((HeadHead++))
+        elif [ ${doublet[@]} -eq 1 -a ${doublet1[@]} -eq 0 ]
+        then
+           echo "Head Tail"
+           ((HeadTail++))
+        elif [ ${doublet[@]} -eq 0 -a ${doublet1[@]} -eq 1 ]
+        then
+           echo "Tail Head"
+           ((TailHead++))
+        elif [ ${doublet[@]} -eq 0 -a ${doublet1[@]} -eq 0 ]
+        then
+           echo "Tail Tail"
+           ((TailTail++))
+        else
+           exit;
+        fi
 done
-   echo ${singlet[@]}
-   echo ${!singlet[@]}
 
+echo "Number of Head Head Win = $HeadHead"
+echo "Number of Head Tail Win = $HeadTail"
+echo "Number of Tail Head Win = $TailHead"
+echo "Number of Tail Tail Win = $TailTail"
 
-for(( i=1; i<=10; i++ ))
-do
-   if [ ${singlet[i]} -eq 1 ]
-   then
-       ((Head++))
-   else
-       ((Tail++))
-   fi
-done
-
-echo "Number of Heads Win = $Head"
-echo "Number of Tails Win = $Tail"
-
-HeadPercentage=$((100*$Head/10))
-echo $HeadPercentage
-TailPercentage=$((100*$Tail/10))
-echo $TailPercentage
+HeadHeadPercentage=$((100*$HeadHead/20))
+echo "Percentage of HeadHead = $HeadHeadPercentage"
+HeadTailPercentage=$((100*$HeadTail/20))
+echo "Percentage of HeadTail = $HeadTailPercentage"
+TailHeadPercentage=$((100*$TailHead/20))
+echo "Percentage of TailHead = $TailHeadPercentage"
+TailTailPercentage=$((100*$TailTail/20))
+echo "Percentage of TailTail = $TailTailPercentage"
